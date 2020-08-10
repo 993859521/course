0cc305f62b6661e8226733fdb5e6bb8d024eb846
993859521 <993859521@qq.com>
2020-08-10 11:02:37 +0800

    feat:
    项目创建
    
    BREAKING CHANGE: 增加了gateway，system，service模块
    
    Signed-off-by: 993859521 <993859521@qq.com>

---
+++ serive/src/test/java/com/course/serive/SeriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.serive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeriveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
