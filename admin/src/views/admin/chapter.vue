6acf277bdb2daac3d0b73498e2fa322569a26954
993859521 <993859521@qq.com>
2020-08-15 11:34:10 +0800

    feat:
    1.大章增加负责老师
    2.课程显示该课程大章里面的所有老师
    
    Signed-off-by: 993859521 <993859521@qq.com>

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -128,7 +128,8 @@
                 let _this = this;
 
                 // 保存校验
-                if (!Validator.require(_this.chapter.name, "名称")
+                if (!Validator.require(_this.chapter.name, "名称")||
+                    !Validator.require(_this.chapter.teacherId, "老师")
                     || !Validator.length(_this.chapter.courseId, "课程ID", 1, 8)) {
                     return;
                 }
